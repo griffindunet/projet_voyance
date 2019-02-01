1dee96c7fae94c1614943616de26ac277637c033
remysimpson <45562736+griffindunet@users.noreply.github.com>
2019-02-01 16:21:36 +0100

    Update tarot.php

--- tarot.php
+++ tarot.php
@@ -6,7 +6,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
     <meta name="author" content="">
-    <link rel="icon" href="../../../../favicon.ico">
   	<link rel="stylesheet" href="css/carte.css" />
 
   	<script src="https://code.jquery.com/jquery-1.10.2.js"></script>
